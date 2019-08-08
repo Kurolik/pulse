9e882f44c295dec2f5680e2245748716eaa8a4ab
“Oleg” <kurolik@live.ru>
2019-08-08 21:33:17 +0300

    Creating section CAROUSEL-adp

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
-alert (1);
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+      speed: 800,
+      adaptiveHeight: false,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      prevArrow: '<button type="button" class="slick-prev"><img src="img/carousel/arrow-left.png"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="img/carousel/arrow-right.png"></button>',
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            dots: true,
+            arrows: false
+      }
+        }
+      ]
+    });
+  });
